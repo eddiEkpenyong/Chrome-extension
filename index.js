e9a17758dc681c400237300fdf8ed66f0ed3fe04
eddiEkpenyong <edidiong.ekpenyong.19@gmail.com>
2022-07-13 22:33:02 +0100

    SaveLater Extension

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 let myLeads = []
+
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ulel")
-let listItems = ""
+const delBtn = document.getElementById("delete-btn")
+const saveBtn = document.getElementById("save-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function() {
+
+saveBtn.addEventListener("click", function() {
+    chrome.tabs.query({active: true}, function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
+    })
+})
+
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+//refactoring the render function so it could act dynamically
+function render(array) {
+    let listItems = ""
+    for (let i = 0; i < array.length; i++) {
+        listItems += `
+             <li>
+                <a target = '_blank' href = '${array[i]}'>
+                    ${array[i]}
+                </a>
+            </li>
+        `
+    }
+
+    ulEl.innerHTML = listItems
+}
+
+
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+})
+
+delBtn.addEventListener("dblclick", function () {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
 })
